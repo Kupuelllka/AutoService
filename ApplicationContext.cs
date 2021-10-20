f4ff44d9ef19c10b7d1b81b34aa8d6d4c7c32c3c
Kupuellllka <super.dima8921@gmail.com>
2021-10-20 20:31:19 +0300

    BEg

---
+++ ApplicationContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Data.Identity;
+namespace UserApp
+{
+     class ApplicationContext:DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public ApplicationContext():base("DefaultConnection") { }
+    }
+}
---
+++ Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace UsersApp { 
+ class User
+ {
+        private int Id { get; set; }
+        private string FullName;
+        private string PhoneNumber;
+
+        public User() { }
+        public User(int id,string FullName, string PhoneNumber) {
+            this.Id = id;
+            this.FullName = FullName;
+            this.PhoneNumber = PhoneNumber; 
+        }
+ }
+}
